ca0de0f516693971cabd96e6b2069a57d2c2e636
John Pioc <Johnpioc15@gmail.com>
2023-02-09 14:41:32 +1000

    created staking contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -10,14 +10,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-error CallerIsNotUser();
-error InvalidWhitelistAllocation();
-error MintingTooMany();
-error InsufficientFunds();
-error MintingOverCollectionSize();
-error SaleIsClosed();
-error MintingOverWhitelistAllocation();
-error InvalidProof();
+error NFT__CallerIsNotUser();
+error NFT__InvalidWhitelistAllocation();
+error NFT__MintingTooMany();
+error NFT__InsufficientFunds();
+error NFT__MintingOverCollectionSize();
+error NFT__SaleIsClosed();
+error NFT__MintingOverWhitelistAllocation();
+error NFT__InvalidProof();
 
 contract NFT is ERC721A, Ownable {
 
@@ -43,7 +43,7 @@ contract NFT is ERC721A, Ownable {
 
     /// @notice Modifier to verify that caller doesn't come from a contract
     modifier callerIsUser() {
-        if (tx.origin != msg.sender) revert CallerIsNotUser();
+        if (tx.origin != msg.sender) revert NFT__CallerIsNotUser();
         _;
     }
 
@@ -55,10 +55,10 @@ contract NFT is ERC721A, Ownable {
     /// @notice Function to mint NFTs during the public sale
     /// @param _mintAmount Number of NFTs to mint
     function publicMint(uint64 _mintAmount) public payable callerIsUser {
-        if (_numberMinted(msg.sender) - _getAux(msg.sender) + _mintAmount > publicMaxMintAmount) revert MintingTooMany();
-        if (totalSupply() + _mintAmount > collectionSize) revert MintingOverCollectionSize();
-        if (saleState != SaleState.PUBLIC) revert SaleIsClosed();
-        if (msg.value < _mintAmount * publicMintPrice) revert InsufficientFunds();
+        if (_numberMinted(msg.sender) - _getAux(msg.sender) + _mintAmount > publicMaxMintAmount) revert NFT__MintingTooMany();
+        if (totalSupply() + _mintAmount > collectionSize) revert NFT__MintingOverCollectionSize();
+        if (saleState != SaleState.PUBLIC) revert NFT__SaleIsClosed();
+        if (msg.value < _mintAmount * publicMintPrice) revert NFT__InsufficientFunds();
 
         _safeMint(msg.sender, _mintAmount);
     }
@@ -67,13 +67,13 @@ contract NFT is ERC721A, Ownable {
     /// @param _merkleProof Merkle Proof for caller's address
     /// @param _mintAmount Number of NFTs to mint
     function whitelistMint(bytes32[] calldata _merkleProof, uint64 _mintAmount) public payable callerIsUser {
-        if (_getAux(msg.sender) + _mintAmount > whitelistMaxMintAmount) revert MintingTooMany();
-        if (totalSupply() + _mintAmount > whitelistAllocation) revert MintingOverWhitelistAllocation();
-        if (saleState != SaleState.WHITELIST) revert SaleIsClosed();
-        if (msg.value < _mintAmount * whitelistMintPrice) revert InsufficientFunds();
+        if (_getAux(msg.sender) + _mintAmount > whitelistMaxMintAmount) revert NFT__MintingTooMany();
+        if (totalSupply() + _mintAmount > whitelistAllocation) revert NFT__MintingOverWhitelistAllocation();
+        if (saleState != SaleState.WHITELIST) revert NFT__SaleIsClosed();
+        if (msg.value < _mintAmount * whitelistMintPrice) revert NFT__InsufficientFunds();
 
         bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
-        if(!(MerkleProof.verify(_merkleProof, whitelistMerkleRoot, leaf))) revert InvalidProof();
+        if(!(MerkleProof.verify(_merkleProof, whitelistMerkleRoot, leaf))) revert NFT__InvalidProof();
 
         _setAux(msg.sender, _getAux(msg.sender) + _mintAmount);
         _safeMint(msg.sender, _mintAmount);
@@ -104,7 +104,7 @@ contract NFT is ERC721A, Ownable {
     /// @dev Only owner can call this function. New whitelist allocation cannot be greater than collection size
     /// @param _whitelistAllocation New whitelist allocation
     function setWhitelistAllocation(uint256 _whitelistAllocation) public onlyOwner {
-        if (_whitelistAllocation > collectionSize) revert InvalidWhitelistAllocation();
+        if (_whitelistAllocation > collectionSize) revert NFT__InvalidWhitelistAllocation();
         whitelistAllocation = _whitelistAllocation;
     }
 
---
+++ contracts/NFTStaking.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "erc721a/contracts/IERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+
+error NFTStaking__ContractNotApproved();
+error NFTStaking__TokenAlreadyStaked();
+error NFTStaking__TransferFailed();
+error NFTStaking__StakingNotOpen();
+
+contract NFTStaking is ERC721Holder, Ownable {
+
+    struct Stake {
+        uint256 timestamp;
+        address owner;
+    }
+
+    IERC721A public nftAddress;
+    bool public isStakingOpen;
+
+    event NFTStaked(address owner, uint256 tokenId, uint256 timestamp);
+    event NFTUnstaked(address owner, uint256 tokenId, uint256 timestamp);
+
+    mapping(uint256 => Stake) public vault;
+
+    constructor(address _nftAddress) {
+        nftAddress = IERC721A(_nftAddress);
+        isStakingOpen = false;
+    }
+
+    function stakeMany(uint256[] calldata _tokenIds) public {
+        if (!isStakingOpen) revert NFTStaking__StakingNotOpen();
+        if (!nftAddress.isApprovedForAll(msg.sender, address(this))) revert NFTStaking__ContractNotApproved();
+
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            uint256 tokenId = _tokenIds[i];
+            if (nftAddress.ownerOf(tokenId) == msg.sender) {
+                vault[tokenId] = Stake({
+                    timestamp: uint256(block.timestamp),
+                    owner: msg.sender
+                });
+
+                nftAddress.transferFrom(msg.sender, address(this), tokenId);
+                emit NFTStaked(msg.sender, tokenId, block.timestamp);
+            }
+        }
+    }
+
+    function unstakeMany(uint256[] calldata _tokenIds) public {
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            uint256 tokenId = _tokenIds[i];
+            Stake memory stakeDetails = vault[tokenId];
+            if (stakeDetails.owner == msg.sender) {
+                delete vault[tokenId];
+                nftAddress.transferFrom(address(this), msg.sender, tokenId);
+                emit NFTUnstaked(msg.sender, tokenId, block.timestamp);
+            }
+        }
+    }
+
+    function forceUnstakeMany(uint256[] calldata _tokenIds) public onlyOwner {
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            uint256 tokenId = _tokenIds[i];
+            Stake memory stakeDetails = vault[tokenId];
+            delete vault[tokenId];
+            nftAddress.transferFrom(address(this), stakeDetails.owner, tokenId);
+            emit NFTUnstaked(msg.sender, tokenId, block.timestamp);
+        }
+    }
+
+    function setNftAddress(address _nftAddress) public onlyOwner {
+        nftAddress = IERC721A(_nftAddress);
+    }
+
+    function toggleStakingOpen() public onlyOwner {
+        isStakingOpen = !isStakingOpen;
+    }
+}
